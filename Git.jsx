6b0aa29f9cf0c8f5f84743d0eed746a08d1009c7
Mayank Nagle <mayank.nagle19@gmail.com>
2023-05-15 17:59:30 +0530

    initial commit

---
+++ Git.jsx
@@ -0,0 +1,52 @@
+// Git - It is a version control system, save all the history of the files and also the changes made in it. Git achieves multiple things through it. 
+/* 
+Git is different from GitHub
+ 
+Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+
+Open GitBash and type
+---------------------
+- To check the version of Git => git --version  
+2.40.1
+- To set config to tell your name => git config --global user.name "Mayank Nagle"
+- To set config to tell your email id => git config --global user.email "mayank.nagle19@gmail.com"
+- To check whether it is actually did or not => 
+for name => git config --global user.name
+Mayank Nagle
+for email => git config --global user.email
+mayank.nagle19@gmail.com
+- For editing config files.
+=> git config --global --edit
+insert -> edit your data
+and escape after save and write :wq to exit there.
+
+for creating git file
+---------------------
+- mkdir name => for making new folder.
+- cd name => for change directory.
+- cd d: => for changing directory to d
+- cd f: => for changing directory to f
+- cd .. => for returning to desktop.
+- cd ~ => for goining in root directory.
+- clear => for clearing data
+- cd "Web Development" => Space taken inside the quotes singlr or double
+- cd developemnt => without in quotes beacuse there is no space in the spacific folder
+
+example :-
+cd d: 
+cd "Web Development"
+cd "Git and Github"
+
+Now we can make our repository in it. Repository means the project that can be tracked through git.
+
+- if you want to make this folder a git repo, then write => git init
+for creating a repository.
+- ls => to find repository empty or not.
+- ls => gives the directories in the specific drive.
+- ls -a => then you can see a file .git, this file is created in it when we make a new repository. which you know that it contains the files which is being tracked by git.
+
+
+Now creating a file.
+- git init => if you want to make this folder a git repository.
+- git status => which tells you the status about the changes made in the repository, what modifications have been done, which file have been deleted which files have been changed.
+*/
---
+++ sum.js
@@ -0,0 +1,4 @@
+let a =4;
+let b=3;
+let sum=a+b;
+console.log(sum);
