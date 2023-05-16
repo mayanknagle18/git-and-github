d0452758caff585caf2d3cf64211d4d1b17a3f43
Mayank Nagle <mayank.nagle19@gmail.com>
2023-05-16 13:21:25 +0530

    message added multiply.js file

--- Git.jsx
+++ Git.jsx
@@ -47,6 +47,58 @@ for creating a repository.
 
 
 Now creating a file.
+---------------------
 - git init => if you want to make this folder a git repository.
 - git status => which tells you the status about the changes made in the repository, what modifications have been done, which file have been deleted which files have been changed.
+- git add file_name => if we do git add sum.js then our file start to be tracked.
+
+staging - When we got untracked files in git status then we have to add files in it.
+
+Untracked files:
+(use "git add <file>..." to include in what will be committed)
+Git.jsx   
+sum.js
+
+Changes to be committed:
+(use "git rm --cached <file>..." to unstage)
+new file:   Git.jsx        
+new file:   sum.js
+
+now this file is in under staging area.
+
+Staging Area - It is the area where you can hold your changes before finally committing.
+If you want 5-6 files in commit so you'll put those files in the staging. and from there your files will be committed.
+
+Working Directory ----> Staging Area ----> Repository
+git add          git commit
+
+- git commit -m "initial commit" => To commit the command initial commit
+- git log => if you want to see the amount of your commits until now.
+
+example - 
+Author: Mayank Nagle <mayank.nagle19@gmail.com>
+Date:   Mon May 15 17:59:30 2023 +0530
+initial commit
+
+All commit have different hashcode generated
+- git commit -m "message"
+- cmd+k or clear => clear the screen
+
+example - git add subtract.js
+- git status => new file 
+modified files => Git.jsx, sum.js
+
+- git commit -m "adding subtract.js" one file changed
+- git log
+
+commit - different hexacode
+adding subtract.js
+
+commit - hexacode
+initial commit
+--------------------------------------------------------
+- git add . => (.) dot all the files will come in the staging area. 
+
+example - git commit -m "message added in file."
+
 */
---
+++ multiply.js
@@ -0,0 +1,4 @@
+let a = 5;
+let b = 6;
+let multiply = a*b;
+console.log(multiply);
