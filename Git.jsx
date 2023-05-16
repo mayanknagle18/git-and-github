40586257da147cdd00b0a20ffd36b6a9ff10f9a2
Mayank Nagle <mayank.nagle19@gmail.com>
2023-05-16 15:30:48 +0530

    git mesage

--- Git.jsx
+++ Git.jsx
@@ -31,6 +31,7 @@ for creating git file
 - clear => for clearing data
 - cd "Web Development" => Space taken inside the quotes singlr or double
 - cd developemnt => without in quotes beacuse there is no space in the spacific folder
+- ctrl + c => to out from the new terminal.
 
 example :-
 cd d: 
@@ -47,6 +48,102 @@ for creating a repository.
 
 
 Now creating a file.
+---------------------
 - git init => if you want to make this folder a git repository.
 - git status => which tells you the status about the changes made in the repository, what modifications have been done, which file have been deleted which files have been changed.
+- git add file_name => if we do git add sum.js then our file start to be tracked.
+
+staging - When we got untracked files in git status then we have to add files in it.
+
+Untracked files:
+(use "git add <file>..." to include in what will be committed)
+Git.jsx   
+sum.js
+
+Changes to be committed:
+(use "git rm --cached <file>..." to unstage)
+new file:   Git.jsx        
+new file:   sum.js
+
+now this file is in under staging area.
+
+Staging Area - It is the area where you can hold your changes before finally committing.
+If you want 5-6 files in commit so you'll put those files in the staging. and from there your files will be committed.
+
+Working Directory ----> Staging Area ----> Repository
+git add          git commit
+
+- git commit -m "initial commit" => To commit the command initial commit
+- git log => if you want to see the amount of your commits until now.
+
+example - 
+Author: Mayank Nagle <mayank.nagle19@gmail.com>
+Date:   Mon May 15 17:59:30 2023 +0530
+initial commit
+
+All commit have different hashcode generated
+- git commit -m "message"
+- cmd+k or clear => clear the screen
+
+example - git add subtract.js
+- git status => new file 
+modified files => Git.jsx, sum.js
+
+- git commit -m "adding subtract.js" one file changed
+- git log
+
+commit - different hexacode
+adding subtract.js
+
+commit - hexacode
+initial commit
+--------------------------------------------------------
+- git add . => (.) dot all the files will come in the staging area. 
+
+example - git commit -m "message added in file."
+
+if you want to go to first commit, when initial commit was done for that we use checkout command
+- git checkout hashcode_name => give you a first commit.
+- git checkout master => give you back to the master and all the files are recovered.
+
+- git branch => give you the initial master branch, and now you can make multiple branches
+example - git branch 
+(HEAD detached at 6b0aa29)
+- git log 
+6b0aa29 (head)
+- git master 
+master
+- git log
+head to master
+-------------------------------------------------------------------
+Master Branch ---> Develop Branch
+master branch is the main branch you can make multiple branches from it.
+
+- git branch branch_name => for making a branch in master branch
+example - git branch develop
+- git branch
+=> gives us two branches - develop and master
+- git checkout develop => switch to branch develop
+
+- git checkout -b branch_name => for this you can make branch as well as you checked out.
+example - git checkout -b mayank/multiply => here branch name is person_name/task_performed_by_him
+- git status
+modified files :- Git.jsx and sum.js
+untracked file :- multiply.js
+not staged :- Git.jsx
+
+- git add multiply.js
+- git commit -m "message added this file"
+- git add Git.jsx
+- git status
+
+- git checkout develop => please commit your change
+- git commit -m "message develop"
+- git checkout develop
+
+develop branch does not know that multiply.js was even created and some work was done over there.
+
+for merging files develop and mayank/multiply
+---------------------------------------------
+- git merge mayank/multiply
 */
